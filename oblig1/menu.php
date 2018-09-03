5dde7f44af4c4b8739e6ac144ed9b2e1ccaa1438
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2018-09-03 16:33:49 +0200

    added menu file

---
+++ oblig1/menu.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Oblig 1 - webutvikling</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="stilark.css">
+	</head>
+	<body>
+		<h1>Line Sharina Aamodt Hagen</h1>
