932bd01891e5a49cf0707fe10cf267379331eda2
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2018-09-03 21:44:01 +0200

    Added and styled navbar

---
+++ oblig1/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Oblig 1 - webutvikling</title>
+	<meta charset="utf-8" />
+	 <meta name="viewport" content="initial-scale=1, width=device-width, height=device-height">
+	<link rel="stylesheet" type="text/css" href="css/stilark.css">
+</head>
+<body>
+	<header>
+		<div class="header-img">
+			<div class="header-img-content">
+				<h1>Oblig 1</h1>
+				<nav class="navbar">
+					<a href="#">Oppgave 2</a>
+					<a href="#">Oppgave 3</a>
+					<a href="#">Oppgave 4</a>
+					<a href="#">Oppgave 5</a>
+					<a href="#">Oppgave 6</a>
+				</nav>
+			</div>
+		</div>
+	</header>
