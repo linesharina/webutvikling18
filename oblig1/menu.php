e9dfd44183b50781759f8e76e60d2969a372b3ad
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2018-09-03 20:49:43 +0200

    changed filename to header

--- oblig1/menu.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Oblig 1 - webutvikling</title>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="stilark.css">
-	</head>
-	<body>
-		<h1>Line Sharina Aamodt Hagen</h1>
