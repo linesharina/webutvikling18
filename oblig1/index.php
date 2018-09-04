6d359a1aceebee52a8a0aa1be55354a43bb8f329
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2018-09-05 00:16:24 +0200

    Added articles

--- oblig1/index.php
+++ oblig1/index.php
@@ -1,5 +1,44 @@
 <?php require('header.php') ?>
-	
-	<p>Hva er det du vil????</p>
-
+	<main class="articles">
+		<h2>Oblig 1</h2>
+		<section class="articles-inner">
+			<article>
+				<img src="img/test1.jpg">
+				<h3>Oppgave 2</h3>
+				<p>Laget en HTML fil som inneholder strukturkode for en enkel artikkel.</p>
+				<a href="#" class="button">Les mer</a>
+			</article>
+			<article>
+				<img src="img/test2.jpg">
+				<h3>Oppgave 3</h3>
+				<p>Beskrivelse av hvorfor det er viktig å skille mellom innhold og design.</p>
+				<a href="#" class="button">Les mer</a>
+			</article>
+			<article>
+				<img src="img/test4.jpg">
+				<h3>Oppgave 4</h3>
+				<p>Artikkel om webservere. Hva er en webserver? Hvilke typer finnes? Funksjonalitet?</p>
+				<a href="#" class="button">Les mer</a>
+			</article>
+			<article>
+				<img src="img/test1.jpg">
+				<h3>Oppgave 5</h3>
+				<p>De strukturelle/semantiske taggene i HTML.</p>
+				<a href="#" class="button">Les mer</a>
+			</article>
+			<article>
+				<img src="img/test1.jpg">
+				<h3>Oppgave 6</h3>
+				<p>Oppgave om eksternt stilsett.</p>
+				<a href="#" class="button">Les mer</a>
+			</article>
+			<article>
+				<img src="img/test1.jpg">
+				<h3>Oppgave 7</h3>
+				<p>Testing av box modell-prinsippet.</p>
+				<a href="#" class="button">Les mer</a>
+			</article>
+		</section>
+	</main>
+		
 <?php require('footer.php') ?>
