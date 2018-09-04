a5cde485d71dda329c2adf0b1a953208715fdfaa
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2018-09-05 00:40:10 +0200

    Added article file

---
+++ oblig1/artikkel.php
@@ -0,0 +1,11 @@
+<?php require('header.php') ?>
+	<main class="articles">
+		<h2>Oblig 1</h2>
+		<article class="article">
+			<h3>Oppgave 2</h3>
+			<p>Dato utført: 05.09.2018</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo arcu non elit elementum fermentum. Suspendisse finibus velit quis feugiat blandit. Mauris velit quam, tincidunt eget molestie sed, condimentum pellentesque ligula.</p>
+		</article>
+	</main>
+		
+<?php require('footer.php') ?>
