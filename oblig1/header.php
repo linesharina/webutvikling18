10ee11071fa53b5efcb6e37a91763e90c13ec1ad
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2018-09-05 00:18:51 +0200

    Header - grammar

--- oblig1/header.php
+++ oblig1/header.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Oblig 1 - webutvikling</title>
+	<title>Webutvikling - Oblig 1</title>
 	<meta charset="utf-8" />
 	 <meta name="viewport" content="initial-scale=1, width=device-width, height=device-height">
 	<link rel="stylesheet" type="text/css" href="css/stilark.css">
+	<link href="https://fonts.googleapis.com/css?family=Nothing+You+Could+Do|Playfair+Display" rel="stylesheet">
+	<link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
 </head>
 <body>
 	<header>
 		<div class="header-img">
 			<div class="header-img-content">
-				<h1>Oblig 1</h1>
+				<h1>Line Sharina Aamodt</h1>
 				<nav class="navbar">
 					<a href="#">Oppgave 2</a>
 					<a href="#">Oppgave 3</a>
