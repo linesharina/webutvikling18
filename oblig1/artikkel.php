a4f5b2376072fd8c79b6f349be082f712d9fea65
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2018-09-05 17:43:10 +0200

    added text in oppgave2

--- oblig1/artikkel.php
+++ oblig1/artikkel.php
@@ -4,7 +4,33 @@
 		<article class="article">
 			<h3>Oppgave 2</h3>
 			<p>Dato utført: 05.09.2018</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo arcu non elit elementum fermentum. Suspendisse finibus velit quis feugiat blandit. Mauris velit quam, tincidunt eget molestie sed, condimentum pellentesque ligula.</p>
+			<p>I denne oppgaven skulle vi lage en HTML-fil som inneholder strukturkode for en enkel artikkel. 
+			Denne skal benyttes videre i de andre oppgavene for å gjøre sidene uniforme.</p>
+			<h4>Minimumskrav</h4>
+			<p>Følgende punkter var minimumskravet for å få oppgavene godkjent:</p>
+			<ul>
+				<li>En enkel header med følgende innhold:
+					<ul>
+						<li>En tittel</li>
+						<li>En enkel meny som linker til de forskjellige oppgavene og hovedsiden</li>
+					</ul>
+				</li>
+				<li>Et hovedområde som skal ha følgende innhold:
+					<ul>
+						<li>En overskrift</li>
+						<li>Dato for når oppgaven er gjort</li>
+						<li>Et illustrasjonsfoto</li>
+						<li>Tekstlig innhold</li>
+					</ul>
+				</li>
+				<li>En enkel footer med følgende innhold:
+					<ul>
+						<li>Årstall</li>
+						<li>Navn og e-post adresse</li>
+					</ul>
+				</li>
+			</ul>
+			<p>Resultatet/strukturen ser du på denne siden, samt resten av oppgavene.</p>
 			<img src="img/illustration.jpg" alt="Illustrasjonsbilde">
 		</article>
 	</main>
