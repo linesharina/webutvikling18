b1b32b2cd35972450cf67c57d1af62f4c9bfad45
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2018-09-05 17:43:35 +0200

    added alt attributt

--- oblig1/index.php
+++ oblig1/index.php
@@ -1,39 +1,41 @@
 <?php require('header.php') ?>
 	<main class="articles">
-		<h2>Oblig 1</h2>
+		<header>
+			<h2>Oblig 1</h2>
+		</header>
 		<section class="articles-inner">
 			<article>
-				<img src="img/test1.jpg">
+				<img src="img/test1.jpg" alt="oppgave 2">
 				<h3>Oppgave 2</h3>
 				<p>Laget en HTML fil som inneholder strukturkode for en enkel artikkel.</p>
 				<a href="/webutvikling18/oblig1/artikkel.php" class="button">Les mer</a>
 			</article>
 			<article>
-				<img src="img/test2.jpg">
+				<img src="img/test2.jpg" alt="oppgave 3">
 				<h3>Oppgave 3</h3>
 				<p>Beskrivelse av hvorfor det er viktig å skille mellom innhold og design.</p>
 				<a href="#" class="button">Les mer</a>
 			</article>
 			<article>
-				<img src="img/test4.jpg">
+				<img src="img/test4.jpg" alt="oppgave 4">
 				<h3>Oppgave 4</h3>
-				<p>Artikkel om webservere. Hva er en webserver? Hvilke typer finnes? Funksjonalitet?</p>
+				<p>Artikkel om webservere. Hva er en webserver? Hvilke typer finnes?</p>
 				<a href="#" class="button">Les mer</a>
 			</article>
 			<article>
-				<img src="img/test1.jpg">
+				<img src="img/test1.jpg" alt="oppgave 5">
 				<h3>Oppgave 5</h3>
-				<p>De strukturelle/semantiske taggene i HTML.</p>
+				<p>Strukturelle/semantiske tagger i HTML.</p>
 				<a href="#" class="button">Les mer</a>
 			</article>
 			<article>
-				<img src="img/test1.jpg">
+				<img src="img/test1.jpg" alt="oppgave 6">
 				<h3>Oppgave 6</h3>
 				<p>Oppgave om eksternt stilsett.</p>
 				<a href="#" class="button">Les mer</a>
 			</article>
 			<article>
-				<img src="img/test1.jpg">
+				<img src="img/test1.jpg" alt="oppgave 7">
 				<h3>Oppgave 7</h3>
 				<p>Testing av box modell-prinsippet.</p>
 				<a href="#" class="button">Les mer</a>
