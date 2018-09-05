ab88f9a26491b13efcce229c034be335b90a25e9
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2018-09-05 17:42:40 +0200

    added illustration img in article page

--- oblig1/artikkel.php
+++ oblig1/artikkel.php
@@ -5,6 +5,7 @@
 			<h3>Oppgave 2</h3>
 			<p>Dato utført: 05.09.2018</p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo arcu non elit elementum fermentum. Suspendisse finibus velit quis feugiat blandit. Mauris velit quam, tincidunt eget molestie sed, condimentum pellentesque ligula.</p>
+			<img src="img/illustration.jpg" alt="Illustrasjonsbilde">
 		</article>
 	</main>
 		
