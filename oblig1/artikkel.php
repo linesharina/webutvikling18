80164da90ffe3c4574db3122feec66e3de8ac9ff
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2018-09-05 20:34:33 +0200

    Added left and right articles

--- oblig1/artikkel.php
+++ oblig1/artikkel.php
@@ -2,36 +2,44 @@
 	<main class="articles">
 		<h2>Oblig 1</h2>
 		<article class="article">
-			<h3>Oppgave 2</h3>
-			<p>Dato utført: 05.09.2018</p>
-			<p>I denne oppgaven skulle vi lage en HTML-fil som inneholder strukturkode for en enkel artikkel. 
-			Denne skal benyttes videre i de andre oppgavene for å gjøre sidene uniforme.</p>
-			<h4>Minimumskrav</h4>
-			<p>Følgende punkter var minimumskravet for å få oppgavene godkjent:</p>
-			<ul>
-				<li>En enkel header med følgende innhold:
-					<ul>
-						<li>En tittel</li>
-						<li>En enkel meny som linker til de forskjellige oppgavene og hovedsiden</li>
-					</ul>
-				</li>
-				<li>Et hovedområde som skal ha følgende innhold:
-					<ul>
-						<li>En overskrift</li>
-						<li>Dato for når oppgaven er gjort</li>
-						<li>Et illustrasjonsfoto</li>
-						<li>Tekstlig innhold</li>
-					</ul>
-				</li>
-				<li>En enkel footer med følgende innhold:
-					<ul>
-						<li>Årstall</li>
-						<li>Navn og e-post adresse</li>
-					</ul>
-				</li>
-			</ul>
-			<p>Resultatet/strukturen ser du på denne siden, samt resten av oppgavene.</p>
-			<img src="img/illustration.jpg" alt="Illustrasjonsbilde">
+			<div class="article-left">
+				<h3>Oppgave 2</h3>
+				<p>Dato utført: 05.09.2018</p>
+				<p>I denne oppgaven skulle vi lage en HTML-fil som inneholder strukturkode for en enkel artikkel. 
+				Denne skal benyttes videre i de andre oppgavene for å gjøre sidene uniforme.</p>
+				<h4>Minimumskrav</h4>
+				<p>Følgende punkter var minimumskravet for å få oppgavene godkjent:</p>
+				<ul>
+					<li>En enkel header med følgende innhold:
+						<ul>
+							<li>En tittel</li>
+							<li>En enkel meny som linker til de forskjellige oppgavene og hovedsiden</li>
+						</ul>
+					</li>
+					<li>Et hovedområde som skal ha følgende innhold:
+						<ul>
+							<li>En overskrift</li>
+							<li>Dato for når oppgaven er gjort</li>
+							<li>Et illustrasjonsfoto</li>
+							<li>Tekstlig innhold</li>
+						</ul>
+					</li>
+					<li>En enkel footer med følgende innhold:
+						<ul>
+							<li>Årstall</li>
+							<li>Navn og e-post adresse</li>
+						</ul>
+					</li>
+				</ul>
+				<p>Resultatet/strukturen ser du på denne siden, samt resten av oppgavene.</p>
+			</div>
+			<div class="article-right">
+				<div class="img-wrapper">
+					<div class="img-wrapper-src">
+						<img src="img/illustration.jpg" alt="Illustrasjonsbilde">
+					</div>
+				</div>
+			</div>
 		</article>
 	</main>
 		
