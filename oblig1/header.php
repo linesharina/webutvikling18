d99a9ad1253b1a349f7921ce4a289ef26cd47403
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2018-09-06 18:32:23 +0200

    Fixed validations error

--- oblig1/header.php
+++ oblig1/header.php
@@ -3,9 +3,11 @@
 <head>
 	<title>Webutvikling - Oblig 1</title>
 	<meta charset="utf-8" />
-	 <meta name="viewport" content="initial-scale=1, width=device-width, height=device-height">
+	<meta name="viewport" content="initial-scale=1, width=device-width, height=device-height">
 	<link rel="stylesheet" type="text/css" href="css/stilark.css">
-	<link href="https://fonts.googleapis.com/css?family=Nothing+You+Could+Do|Playfair+Display|Lato" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Nothing+You+Could+Do" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 	<link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
 </head>
 <body>
@@ -14,7 +16,7 @@
 			<div class="header-img-content">
 				<h1>Line Sharina Aamodt</h1>
 				<nav class="navbar">
-					<a href="/webutvikling18/oblig1/artikkel.php">Oppgave 2</a>
+					<a href="artikkel.php">Oppgave 2</a>
 					<a href="#">Oppgave 3</a>
 					<a href="#">Oppgave 4</a>
 					<a href="#">Oppgave 5</a>
